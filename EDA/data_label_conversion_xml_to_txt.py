1146a4d8ec061e8ca0e619fc8d3b3312f061d0a6
jinjuuk <jinjuuk2@gmail.com>
2024-07-11 12:13:16 +0900

    2 files are updated

--- EDA/data_label_conversion_xml_to_txt.py
+++ EDA/data_label_conversion_xml_to_txt.py
@@ -22,15 +22,14 @@ def convert_voc_to_yolo(xml_file, yolo_file, class_names):
             xmlbox = obj.find('bndbox')
             b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text),
                  float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
-            bb = ((b[0] + b[1]) / 2.0 - 1, (b[2] + b[3]) / 2.0 - 1,
-                  b[1] - b[0], b[3] - b[2])
-            bb = (bb[0] / width, bb[1] / height, bb[2] / width, bb[3] / height)
+            bb = ((b[0] + b[1]) / 2.0 / width, (b[2] + b[3]) / 2.0 / height,
+                  (b[1] - b[0]) / width, (b[3] - b[2]) / height)
             f.write(f"{cls_id} {' '.join([str(a) for a in bb])}\n")
 
 # 데이터셋 경로 및 클래스 목록 설정
 xml_folder = '/home/jinjuuk/Downloads/new_datasets'  # XML 파일들이 있는 폴더 경로
-yolo_folder = '/home/jinjuuk/Downloads/new_datasets_txt'  # YOLO 형식의 .txt 파일을 저장할 폴더 경로
-classes = ['cup', 'star']  # 클래스 이름 목록
+yolo_folder = '/home/jinjuuk/Downloads/TEST'  # YOLO 형식의 .txt 파일을 저장할 폴더 경로
+classes = ['star', 'cup']  # 클래스 이름 목록
 
 # 폴더 생성
 os.makedirs(yolo_folder, exist_ok=True)
@@ -41,3 +40,9 @@ for xml_file in os.listdir(xml_folder):
         xml_path = os.path.join(xml_folder, xml_file)
         yolo_path = os.path.join(yolo_folder, xml_file.replace('.xml', '.txt'))
         convert_voc_to_yolo(xml_path, yolo_path, classes)
+
+
+
+
+
+
